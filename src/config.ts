8d1fd71f52f505f3dca87b2d9f3bc94c43bcf320
Linxuan Chen <Linxuan.chen@accuenergy.com>
2024-07-24 17:36:21 -0400

    chore: add http proxy middleware

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-const BASE_URL = process.env.REACT_APP_API_HOSTNAME;
-
-export default BASE_URL;
---
+++ src/setupProxy.js
@@ -0,0 +1,22 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        '/api', 
+        createProxyMiddleware({
+            target: 'http://127.0.0.1:8000/api',
+            changeOrigin: true,
+            logLevel: 'debug', // 启用调试日志以便于调试
+            pathRewrite: (path, req) => path, // 确保请求路径保持不变
+        })
+    );
+    app.use(
+        '/account', 
+        createProxyMiddleware({
+            target: 'http://127.0.0.1:8000/account',
+            changeOrigin: true,
+            logLevel: 'debug', // 启用调试日志以便于调试
+            pathRewrite: (path, req) => path, // 确保请求路径保持不变
+        })
+    );
+};
