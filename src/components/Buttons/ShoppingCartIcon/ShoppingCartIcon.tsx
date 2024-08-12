e4c46c1e163a784f295a931c27f0218ff836ce10
Linxuan Chen <Linxuan.chen@accuenergy.com>
2024-08-12 00:41:11 -0400

    docs(ShoppingCartIcon): update comments

--- src/components/Buttons/ShoppingCartIcon/ShoppingCartIcon.tsx
+++ src/components/Buttons/ShoppingCartIcon/ShoppingCartIcon.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import styles from './ShoppingCartIcon.module.scss';
 type Props = {
     itemCount: Number;
 };
 
+/**
+ * @description: This component renders the shopping cart icon with cart item count displayed in top
+ * nav bar
+ * @param {Props} itemCount number displayed above cart icon to indicate item count
+ */
 export default function ShoppingCartIcon({ itemCount }: Props) {
     return (
         <>
