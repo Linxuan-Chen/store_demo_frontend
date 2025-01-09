9cbabbe6485ec600f07c781f5d9af41b572a8bbe
LinxuanChen <moshouclxclx@163.com>
2025-01-09 19:14:53 +0000

    chore: add api mapping to nginx

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,8 +6,8 @@ module.exports = function(app) {
         createProxyMiddleware({
             target: 'http://127.0.0.1:8000/api',
             changeOrigin: true,
-            logLevel: 'debug', // 启用调试日志以便于调试
-            pathRewrite: (path, req) => path, // 确保请求路径保持不变
+            logLevel: 'debug',
+            pathRewrite: (path, req) => path,
         })
     );
     app.use(
@@ -15,8 +15,8 @@ module.exports = function(app) {
         createProxyMiddleware({
             target: 'http://127.0.0.1:8000/account',
             changeOrigin: true,
-            logLevel: 'debug', // 启用调试日志以便于调试
-            pathRewrite: (path, req) => path, // 确保请求路径保持不变
+            logLevel: 'debug',
+            pathRewrite: (path, req) => path,
         })
     );
     app.use(
